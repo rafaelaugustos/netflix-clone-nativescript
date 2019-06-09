df87d00cf35c8004a46f88e379d602589d96744c
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2019-06-09 02:07:48 -0300

    initial commit

---
+++ app/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <ActionBar 
+    flat
+    backgroundColor="#fff"
+  >
+    <StackLayout>
+      <Image 
+        src="~/assets/images/logo.png" 
+        class="img-header"
+        stretch="aspectFit"
+      />
+    </StackLayout>
+  </ActionBar>
+</template>
+
+<style scoped>
+
+  .img-header{
+    width: 80;
+
+  }
+</style>
---
+++ app/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <Page>
+    <Header />
+      <GridLayout columns="*" rows="*">
+          <Button text="Button" @tap="goPage" />
+      </GridLayout>
+  </Page>
+</template>
+
+<script >
+  import Movie from './Movie'
+  import Header from '@/components/Header'
+  export default {
+    data() {
+      return {
+        msg: 'Home'
+      }
+    },
+    components: { Header }, 
+    methods: {
+      goPage() {
+        this.$navigateTo(Movie);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
